a6e1e07389a35881f0d80067d34c8f77e51ea37d
Brunosp01 <brunospmujurizi@gmail.com>
2025-01-12 08:26:32 +0300

    Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import React from 'https://cdn.skypack.dev/react';
+
+const Footer = () => (
+    <footer>
+        <p>&copy; 2025 PlantGuard. All rights reserved.</p>
+    </footer>
+);
+
+export default Footer;
